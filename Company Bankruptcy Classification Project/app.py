ae71d2e464513eae79115e6248723016619a342c
JackronyK <karani.riungu.a@gmail.com>
2024-06-06 20:38:27 +0300

    App Enhancements
    Added functionality to enable the user selecet either of the 3 models

--- Company Bankruptcy Classification Project/app.py
+++ Company Bankruptcy Classification Project/app.py
@@ -6,29 +6,43 @@ import pickle
 import requests
 from io import BytesIO
 
-# Loading the model
-model_url = 'https://raw.githubusercontent.com/JackronyK/Projects_Jackrony/main/Company%20Bankruptcy%20Classification%20Project/Models/RandomForest.pkl'
-
-# Function to load the model from a URL
-def load_model(url):
-    response = requests.get(url)
+def models_loader(repo_owner, repo_name, path):
+    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{path}"
+    response = requests.get(api_url)
     response.raise_for_status()  # Check if the request was successful
-    model_file = BytesIO(response.content)
-    model = pickle.load(model_file)
-    return model
+    files = response.json()
 
-# Load the model
-try:
-    model = load_model(model_url)
-except Exception as e:
-    st.error(f"Error loading the model: {e}")
-    st.stop()
+    pkl_files = [file for file in files if file['name'].endswith('.pkl')]
+    #Dictonary to store names and content
+    loaded_models = {}
+    for file in pkl_files:
+        file_name = file['name'].replace('.pkl', '')
+        download_url = file['download_url']
 
+        #download the file content
+        response = requests.get(download_url)
+        response.raise_for_status() #check if the request was successfu;
+        file_content = BytesIO(response.content)
+
+        #Load the .pkl file
+        loaded_models[file_name] = pickle.load(file_content)
+
+    return loaded_models
+
+
+#Loading the Models
+repo_owner = 'JackronyK'
+repo_name = 'Projects_Jackrony'
+path = 'Company%20Bankruptcy%20Classification%20Project/Models'
+
+loaded_models = models_loader(repo_owner, repo_name, path)
+        
 # Streamlit app title
 st.title('Bankruptcy Prediction Model')
-
+model_name = st.selectbox('Select a Model You want to use', list(loaded_models.keys()))
 # File uploader for user to upload a CSV file
 upload_file = st.file_uploader('Upload a CSV containing the features', type=["csv"])
+
 
 if upload_file is not None:
     try:
@@ -39,8 +53,8 @@ if upload_file is not None:
         st.write('Uploaded data:')
         st.write(data)
 
-        # Predict using the loaded model
-        predictions = model.predict(data)
+        # Predict using the selected model
+        predictions = loaded_models[model_name].predict(data)
 
         # Adding the predictions to the data file
         data['predictions'] = predictions
